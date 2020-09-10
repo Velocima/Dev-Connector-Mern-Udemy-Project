6a1c886650544d4a1fe581e81add54cee2667ede
Velo <hartley.max@outlook.com>
2020-09-10 20:50:51 +0100

    routers mounted for users, auth, posts and profile at the api endpoint

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const connectToDB = require('./config/db');
 
 const app = express();
+
 connectToDB();
 
 app.get('/', (req, res) => {
     res.send('API Running (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧');
 })
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
 
 const PORT = process.env.PORT || 3000;
 
